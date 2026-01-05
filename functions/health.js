5befb7833530d4d4533b1e4f881559d5887f90c0
Ajun <lin0901210103@gmail.com>
2026-01-06 00:56:29 +0800

    Add Pages Function /health

---
+++ functions/health.js
@@ -0,0 +1,5 @@
+export async function onRequest(context) {
+  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
+    headers: { "content-type": "application/json; charset=utf-8" },
+  });
+}
